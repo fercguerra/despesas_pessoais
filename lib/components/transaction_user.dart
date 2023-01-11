dc77224d929c27ef9fbbeb4d74f4188bc02607e4
Fernanda Guerra <fercguerra06@gmail.com>
2023-01-10 21:22:00 -0300

    Transação #2

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -45,7 +45,7 @@ class _TransactionUserState extends State<TransactionUser> {
     return Column(
       children: [
         TransactionList(_transactions),
-        TransactionForm(),
+        TransactionForm(_addTransaction),
       ],
     );
   }
--- lib/components/transactions_form.dart
+++ lib/components/transactions_form.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class TransactionForm extends StatelessWidget {
-  TransactionForm({Key? key}) : super(key: key);
-  
   final titleController = TextEditingController();
   final valueController = TextEditingController();
+
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit);
 
   @override
   Widget build(BuildContext context) {
@@ -37,8 +39,9 @@ class TransactionForm extends StatelessWidget {
                     ),
                   ),
                   onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0.0;
+                    onSubmit(title, value);
                   },
                 )
               ],
