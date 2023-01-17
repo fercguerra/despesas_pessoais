010c1398fd115509b0fbe195cd43f316e4b55d1b
Fernanda Guerra <fercguerra06@gmail.com>
2023-01-17 20:25:10 -0300

    Convert para Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class ExpensesApp extends StatelessWidget {
   const ExpensesApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    
     return const MaterialApp(home: MyHomePage());
   }
 }
