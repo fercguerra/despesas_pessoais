1e06ac6a6ae57ae29776807b22133b9a2083b35d
Fernanda Guerra <fercguerra06@gmail.com>
2023-01-18 14:07:30 -0300

    Chart #1

---
+++ lib/components/chart.dart
@@ -0,0 +1,46 @@
+import 'package:despesas_pessoais/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:despesas_pessoais/models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+  List<Map<String, Object>> get gruoupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransaction.length; i++) {
+        bool sameDay = recentTransaction[i].date.day == weekDay.day;
+        bool sameMonth = recentTransaction[i].date.month == weekDay.month;
+        bool sameYear = recentTransaction[i].date.year == weekDay.year;
+
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += recentTransaction[i].value;
+        }
+      }
+      print(DateFormat.E().format(weekDay)[0]);
+      print(totalSum);
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
+        'value': totalSum,
+      };
+    });
+  }
+
+  const Chart({Key? key, required this.recentTransaction}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
